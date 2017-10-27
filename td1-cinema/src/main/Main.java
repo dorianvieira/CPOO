1e5d67fedf059fffd15a1b73d24f57a34f10f154
dorianvieira <33154880+dorianvieira@users.noreply.github.com>
2017-10-27 18:11:19 +0200

    td1 cinema v1

---
+++ td1-cinema/src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.logging.Logger;
+
+import model.implementation.SalleCinema;
+
+public final class Main{
+
+ 
+ private static final Logger LOG = Logger.getLogger(Main.class.getCanonicalName());
+
+	public static void main(String[]args){
+
+		
+		SalleCinema salle1 = new SalleCinema();
+		SalleCinema salle2 = new SalleCinema5("Fast and furious",15,150d);
+
+		for(int i=0; i<50; i++){
+		
+		s2.vendrePlace();
+
+		}
+		
+		System.out.println(s2);
+	}
+}
---
+++ td1-cinema/src/model/implementation/SalleCinema.java
@@ -0,0 +1,66 @@
+package implemtation;
+import java.util.logging.Logger;
+import java.lang.String;
+
+public final class SalleCinema{ //final car
+	private static final Logger LOG = Logger.getLogger(SalleCinema.class.getCanonicalName());
+	private String film;
+	static final int nbPlaces; //public car on va l'initialisé direct et la variable bougera pas 
+	private double tarif;
+	private int nbPlacesVendues;
+
+
+	public SalleCinema(String filmProjeté, double prix, int nbPlaces){
+			this.film=filmProjeté;
+			this.tarif=prix;
+			this.nbPlaces=nbPlaces;
+	}
+
+	public SalleCinema(){
+		this.film="Fast and furious";
+		this.prix=15.0d;
+		this.nbPlaces=500;
+		this.nbPlacesVendues=0;
+	}
+
+
+	public void vendrePlace(){
+		nbPlacesVendues++;
+	}
+
+	public double tauxRemplissage(){
+		return (nbPlacesVendues/(double)nbPlaces)*100;
+
+	}
+
+	public double chiffreAffaire(){
+		return (nbPlacesVendues*tarif);
+
+	}
+
+		public int nbPlacesDisponibles(){
+		return (nbPlaces-nbPlacesVendues);
+	}
+
+
+	public void estPleine(){
+		if(nbPlaces==nbPlacesVendues){
+			 LOG.servere("La salle est pleine");
+		}
+		else {
+			LOG.servere("Il reste: " + nbPlacesDisponibles());
+		}
+	}
+
+
+	public String toString(){
+		 StringBuilder sb = new StringBuilder();
+		sb.append("Film Projeté ").append(this.film).append("\n")
+                .append("tarif : ").append(this.tarif).append("\n")
+                .append("Nombre de places : ").append(this.nbPlaces).append("\n");
+                 .append("Taux Remplissage : ").append(tauxRemplissage()).append("%").append("\n");
+                 .append("Chiffre d'affaires: ").append(chiffreAffaire()).append("%").append("\n");
+		 return sb.toString();
+	}
+
+}
