3ab4da313fb0c7a2b48976ebf60225c04b5e28a3
Dorian <33154880+dorianvieira@users.noreply.github.com>
2017-11-23 14:19:26 +0100

    update v2

--- td1-parc/src/main/Main.java
+++ td1-parc/src/main/Main.java
@@ -1,41 +1,37 @@
- package main;
+package main;
 
 import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.logging.Handler;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
 import java.io.IOException;
+import java.util.logging.SimpleFormatter;
 
-import model.declaration.ParcAttractionItf;
-import model.factory.ParcFactory;
-import model.implementation.Parc;
-import model.enumeration.Tarif
-import model.exceptions.PlacesDispoInssufisantes;
+import model.impl.ParcAttraction;
+import model.dec.Tarif;
+import model.exceptions.ParcPlein;
+import model.fact.ParcAttractionFactory;
 
+public final class Main {
+    private static final Logger LOG = Logger.getLogger(Main.class.getCanonicalName());
 
-public class Main {
+    public static void main (String[] args) {
 
-	private static final Logger LOG = Logger.getLogger(Main.class.getCanonicalName());
-
-	public static void main(String[] args){
-
-		try{
-			ParcFactory parc0 = new ParcFactory();
-			
-			SalleCinema parc1 = new Parc("MiageWonderland",180,Tarif.illimite);
-		
-			parc1.getNomParc();
-			
-			parc1.nbPlacesDispo();
-			parc1.vendreBillets(24,Tarif.illimite);
-			parc1.vendreBillets(19,Tarif.illimite5);
-			parc1.vendreBillets(32,Tarif.reduit);
-			parc1.vendreBillets(23,Tarif.reduit5);
-			
-			parc1.chiffreAffaire();
-			parc1.tauxRemplissage();
-
-			System.out.println(parc1.toString());
-			
-		}catch(PlacesDispoInssufisantes ex){
-			LOG.severe(ex.getMessage());
-		}
-	}
-}	
+        ParcAttractionFactory pf = new ParcAttractionFactory();
+        ParcAttraction p1 = pf.createParcAttraction("MiageWonderland",180,38.5d,28.5d);
+        
+        try{
+            p1.vendreBillets(24,Tarif.illimite);
+            p1.vendreBillets(19,Tarif.illimite5);
+            p1.vendreBillets(32,Tarif.reduit);
+            p1.vendreBillets(23,Tarif.reduit5);
+        }catch(ParcPlein ex){
+            LOG.severe(ex.getMessage());
+        }finally{
+            System.out.println(p1.toString());    
+            System.out.println("Le chiffre d'affaires : "+p1.chiffreAffaires()+" euros.");
+            System.out.println("Le taux de remplissage : "+p1.tauxRemplissage()+"%");
+        }
+    }
+}
