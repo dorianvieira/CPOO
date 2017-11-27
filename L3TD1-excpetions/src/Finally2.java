afc90dfbe38f3cb5ac21a3227b11b2ccf830eebf
Dorian <33154880+dorianvieira@users.noreply.github.com>
2017-11-27 19:55:43 +0100

    td1-exceptions final

---
+++ L3TD1-excpetions/src/Finally2.java
@@ -0,0 +1,30 @@
+import java.lang.Exception;
+
+public class Finally2{
+
+	public static void f(int n) throws MonException {
+		try {
+			System.out.println(n);
+			throw new MonException();
+		}catch(MonException e){
+			System.out.println("catch dans f, n = " + n);
+		}
+		finally{
+			System.out.println("dans finally de f, n = " + n);
+		}
+
+	}
+
+
+	public static void main(String[] args){
+		int n=0;
+		try{
+			for(n=1;n<5;n++) f(n);
+		}catch (MonException e){
+			System.out.println("catch dans main - n = " + n);
+		}finally{
+		System.out.println("dans finally de main - n = " + n);
+		}
+	}
+
+}
---
+++ L3TD1-excpetions/src/MonException.java
@@ -0,0 +1,11 @@
+import java.lang.Exception;
+
+public class MonException extends Exception{
+
+	public MonException(){
+		super();
+	}
+	public MonException(String message){
+		super(message);
+	}
+}
